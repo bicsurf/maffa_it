6c0ec4163fa25898b7fb1bb61a53e9a30754ce71
Ionut Adrian <gavrilaionutadrian@gmail.com>
2022-05-01 22:54:32 +0200

    Carousel in view showArticle

--- resources/views/showArticle.blade.php
+++ resources/views/showArticle.blade.php
@@ -1,21 +1,53 @@
 <x-layout>
-    <div class="container">
-        <div class="row">
+    <div class="container my-5">
+        <div class="row ">
             <div class="col-12 col-md-4">
-                <h1>Articolo: {{ $article->title }}</h1>
+                
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <img src="https://picsum.photos/200" class="card-img-top" alt="Immagine Articolo">
-            <div class="col-12 col-md-4">
-                <h5 class="card-title">Titolo: {{ $article->title }}</h5>
-                <p class="card-text">Descrizione: {{ $article->description }}</p>
+    <div class="container-flex">
+        <div  class="row ms-5 my-5">
+            <div  class="col-12 col-md-6">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="https://picsum.photos/1000" class="card-img-top" alt="Immagine Articolo">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://picsum.photos/1000" class="card-img-top" alt="Immagine Articolo">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://picsum.photos/1000" class="card-img-top" alt="Immagine Articolo">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+            
+            <div class="col-12 col-md-3">
+                <h1>{{ $article->title }}</h1>
+                <a href="{{ route('categoryShow', ['category' => $article->category]) }}" class="my-2 border-top pt-2 border-dark card-link shadow btn bg-primary bg-gradient text-light">{{ $article->category->name }}</a>
+
                 <p class="card-text">Prezzo: {{ $article->price }}</p>
-                <a href="{{ route('categoryShow', ['category' => $article->category]) }}" class="my-2 border-top pt-2 border-dark card-link shadow btn bg-primary bg-gradient text-light">Categoria: {{ $article->category->name }}</a>
+                <p class="card-text">Descrizione: {{ $article->description }}</p>
+                
                 <p class="card-text">Pubblicato il: {{ $article->created_at->format('d/m/Y') }} - Autore {{ $article->user->name ?? '' }}</p>
             </div>
         </div>
     </div>
 </x-layout>
+
+

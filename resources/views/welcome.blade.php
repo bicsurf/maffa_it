d71490f05a826c69966337d61fa90eb55a5175ce
Andrea Gomez <andreagh.918@gmail.com>
2022-05-02 12:24:00 +0200

    bnt categorie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,5 +50,30 @@
       </div>
      
       
+<div class="container-fluid my-5">
+  <div class="row justyfi-contend-center">
+    <h1 class="text-center">Categorie</h1>
+    <div class="col-12 col-md-3 botone my-5">
+      <img src="/img/fotografia.jpg" class="rounded-circle btn shadow me-3 d-flex align-items-center" href="#" alt="...">
+      <h5 class="text-center">Fotografia</h5>
+    </div>
+
+    <div class="col-12 col-md-3 botone my-5 ">
+      <img src="https://picsum.photos/200" class="rounded-circle btn shadow me-3 d-flex align-items-center" href="#" alt="...">
+      <h5 class="text-center">Telefonia</h5>
+    </div> 
+    <div class="col-12 col-md-3 botone my-5">
+      <img src="https://picsum.photos/200" class="rounded-circle btn shadow me-3 d-flex align-items-center" href="#" alt="...">
+      <h5 class="text-center">Console e Videogiochi</h5>
+    </div>
+
+    <div class="col-12 col-md-3 botone my-5 " >
+      <img src="/img/audiovideo.jpg" class="img-me rounded-circle btn shadow me-3 d-flex align-items-center my-2"  href="#" alt="...">
+      <h5 class="d-flex align-items-center">Audio e Video</h5>
+    </div>
+  </div>
+</div>
+      
+      
 
 </x-layout>

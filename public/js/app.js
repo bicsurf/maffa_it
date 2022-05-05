70d665c0c1d0408f820f1198759c24789e3e747c
Marco Pedico <marcopedico95@gmail.com>
2022-05-05 17:49:07 +0200

    btnUp

--- public/js/app.js
+++ public/js/app.js
@@ -3235,11 +3235,27 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev"
   }
 });
-var myModal = document.getElementById('myModal');
-var myInput = document.getElementById('myInput');
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus();
-});
+var mybutton = document.getElementById("btn-back-to-top"); // When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+} // When the user clicks on the button, scroll to the top of the document
+
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 /***/ }),
 
--- resources/js/script.js
+++ resources/js/script.js
@@ -17,11 +17,27 @@
       });
 
 
+      let mybutton = document.getElementById("btn-back-to-top");
 
-
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function () {
+        scrollFunction();
+      };
+      
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+      // When the user clicks on the button, scroll to the top of the document
+      mybutton.addEventListener("click", backToTop);
+      
+      function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
